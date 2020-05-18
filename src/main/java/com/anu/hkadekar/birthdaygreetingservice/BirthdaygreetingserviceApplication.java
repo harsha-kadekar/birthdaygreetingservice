7010c5781eca7e0893e7cd7a0ed3e00f8936970b
Harsha Kadekar <harsha.kadekar@gmail.com>
2020-05-17 23:23:47 -0700

    initial project setup

---
+++ src/main/java/com/anu/hkadekar/birthdaygreetingservice/BirthdaygreetingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.anu.hkadekar.birthdaygreetingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdaygreetingserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirthdaygreetingserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anu/hkadekar/birthdaygreetingservice/BirthdaygreetingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anu.hkadekar.birthdaygreetingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdaygreetingserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
