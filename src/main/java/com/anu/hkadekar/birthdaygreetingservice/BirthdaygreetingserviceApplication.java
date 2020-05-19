bbd67653dc4a2f1c8e5e7ec49ac0efb2cfb4429b
Harsha Kadekar <harsha.kadekar@gmail.com>
2020-05-18 23:13:24 -0700

    initial hello world setup

--- src/main/java/com/anu/hkadekar/birthdaygreetingservice/BirthdaygreetingserviceApplication.java
+++ src/main/java/com/anu/hkadekar/birthdaygreetingservice/BirthdaygreetingserviceApplication.java
@@ -1,12 +1,16 @@
 package com.anu.hkadekar.birthdaygreetingservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BirthdaygreetingserviceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BirthdaygreetingserviceApplication.class, args);
     }
 
---
+++ src/main/java/com/anu/hkadekar/birthdaygreetingservice/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.anu.hkadekar.birthdaygreetingservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello! Welcome to Birthday Greetings Service";
+    }
+}
