cbb333e57f2428fabfdc209f2fb383ee4ac34af3
Harsha Kadekar <harsha.kadekar@gmail.com>
2020-05-20 09:16:35 -0700

    trial error commit

--- src/main/java/com/anu/hkadekar/birthdaygreetingservice/controller/HelloController.java
+++ src/main/java/com/anu/hkadekar/birthdaygreetingservice/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello! Welcome to Birthday Greetings Service";
+        return "Hello! Welcome to Birthday Greetings Service!";
     }
 }
